dea056ed671339a023be69a37bb848575beaba7b
Eva Hoerth <hoertheva@gmail.com>
2019-05-10 19:07:23 -0700

    Experiments

---
+++ public/js/bootstrap/sea-cucumber.js
@@ -0,0 +1,21 @@
+(function(){
+	var words = [
+			'Reality',
+			'Culture',
+			'Possibilities',
+			'Friendships',
+			'Change',
+			'Impact',
+			'Mistakes',
+			'Discoveries',
+			'Connections',
+			'Stories',
+			'Better Futures'
+			], i = 0;
+	setInterval(function(){
+			$('#changingword').fadeOut(function(){
+					$(this).html(words[i=(i+1)%words.length]).fadeIn();
+			});
+	}, 3000);
+		
+})();
